5d2ef71aae3a5c961e98d3d6930ac8f74eec4753
keno12345678 <mastercole12345@gmail.com>
2024-05-24 21:48:20 -0500

    ejercicio a

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route :: get('/login', function(){
+    return view('auth.login');
+});
+    Route :: get('/logout', function(){
+    return redirect('/');
+}); 
+    Route :: get('/catalog', function(){
+    return view ('catalog.index');
+}); 
+    Route :: get('/catalog/show/{id}', function($id){
+    return view ('catalog.show',['id'=>$id]);
+});
+Route :: get('/catalog/create', function(){
+    return view ('catalog.create');
+}); 
+    Route :: get('/catalog/edit/{id}', function($id){
+    return view ('catalog.edit',['id'=>$id]);
+});
