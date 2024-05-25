d8aed87204c34a24f45252b285bc9d6aea87a469
keno12345678 <mastercole12345@gmail.com>
2024-05-24 22:04:29 -0500

    e

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-
-
-
-Route::get('/', function () {
     return view('home');
+
 });
+
+
 
 Route :: get('/login', function(){
     return view('auth.login');
